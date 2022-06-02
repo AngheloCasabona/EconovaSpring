050928d8c1824070c52033915f3cfac144608bbe
Fabrizio-2025 <52303635+Fabrizio-2025@users.noreply.github.com>
2022-06-02 09:55:58 -0500

    Update IListVaccinationRepository.java

--- src/main/java/pe/edu/upc/demo/repositories/IListVaccinationRepository.java
+++ src/main/java/pe/edu/upc/demo/repositories/IListVaccinationRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.demo.entities.ListVaccination;
-
+ 
 @Repository
 public interface IListVaccinationRepository extends JpaRepository<ListVaccination, Integer>{
 
