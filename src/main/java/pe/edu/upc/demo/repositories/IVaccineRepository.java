77c451e6d613ee58bfb91a4f18cee69da5a09b1a
Fabrizio-2025 <52303635+Fabrizio-2025@users.noreply.github.com>
2022-06-02 09:55:45 -0500

    Update IVaccineRepository.java

--- src/main/java/pe/edu/upc/demo/repositories/IVaccineRepository.java
+++ src/main/java/pe/edu/upc/demo/repositories/IVaccineRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.demo.entities.Vaccine;
-
+ 
 @Repository
 public interface IVaccineRepository extends JpaRepository<Vaccine, Integer> {
 
